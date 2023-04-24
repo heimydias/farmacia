f9376a3437fdde3e44196c7943ee12da36cb0abe
Heimy Dias <heimysantana@hotmail.com>
2023-04-24 09:03:11 -0300

    novo projeto

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,116 @@
+package com.generation.farmacia.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O Atributo nome é Obrigatório!")
+	private String nome;
+	
+	@NotBlank(message = "O Atributo descricao é Obrigatório!")
+	@Size(max = 1000, message = "O atributo descricao  deve conter no máximo 1000 caracteres")
+	private String descricao;
+	
+	@NotNull
+	@Positive
+	private BigDecimal preco;
+	
+	@NotNull
+	@Positive
+	private int quantidade;
+	
+	@NotNull
+	private String fabricante;
+	
+	private LocalDate dataValidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public LocalDate getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(LocalDate dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+
+}
---
+++ src/main/java/com/generation/farmacia/repository/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.generation.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.farmacia.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	List<Categoria> findAllByMedicamentoContainingIgnoreCase(@Param("medicamento")String Medicamento);
+	
+	List<Categoria> findAllByDermocosmeticoContainingIgnoreCase(@Param("dermocosmetico")String Dermocosmetico);
+
+}
